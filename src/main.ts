9807e7ccde19eaeefbf5499085b71cb9a5f840da
llsr <hklangwang@126.com>
2021-08-12 18:01:46 +0800

    0

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// element plus
+import installElementPlus from '@/plugins/element'
+// 初始化css 重置css默认样式
+import 'normalize.css/normalize.css'
+// 全局 css
+import '@/styles/index.scss'
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(installElementPlus)
+  .mount('#app')
