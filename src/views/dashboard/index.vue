518bf776d7f433ae7f95189987bd0bba3a5de20c
llsr <hklangwang@126.com>
2021-08-26 15:37:13 +0800

    2.4vuex持久化

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -5,24 +5,27 @@
     <svg-icon icon-class="bug"></svg-icon>
     <!-- icon-class svg图标名称 class-name 额外的自定义类名 @click绑定事件 -->
     <svg-icon icon-class="404" class-name="custom-class" @click="sayHi"></svg-icon>
-
+    <h1>测试持久化</h1>
+    <h2 @click="addCount">点击刷新 {{count}}</h2>
   </div>
 </template>
 
 <script lang="ts">
-import { getCurrentInstance, defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from '@/store'
 
 export default defineComponent({
   name: 'Dashboard',
   setup() {
-    // 无法使用ctx 使用proxy来代替
-    // https://blog.csdn.net/qq_39115469/article/details/113817592
-    const { proxy } = getCurrentInstance()
-    const sayHi = () => {
-      proxy.$message.success('恭喜你，这是一条成功消息')
+    const store = useStore()
+
+    const count = computed(() => store.state.test.count)
+    const addCount = () => {
+      store.dispatch('test/addCount')
     }
     return {
-      sayHi
+      count,
+      addCount
     }
   }
 })
